9778b96f4e066d407084d1f2114149c83873a81a
Artem <dradinc@yandex.ru>
2023-04-25 11:16:41 +0300

    DataStore Preferences | DataStore class

---
+++ app/src/main/java/com/example/smartlabs/Common/OnBoardingStatus.kt
@@ -0,0 +1,41 @@
+package com.example.smartlabs.Common
+
+import android.content.Context
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class OnBoardingStatus(context: Context) {
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name="APP_DATA")
+    private var dataStore = context.dataStore
+
+    companion object {
+        val onBoardingStatus = booleanPreferencesKey(name="ON_BOARDING_STATUS")
+    }
+
+    suspend fun setOnBoardingStatus(isOnBoardingStatus: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[onBoardingStatus] = isOnBoardingStatus
+        }
+    }
+
+    fun getOnBoardingStatus(): Flow<Boolean> {
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) { Log.e("DATA_STORE_ERROR", "AAAAAAA") }
+                else { throw exception }
+            }
+            .map {
+                preferences ->
+                val statusOnBoarding = preferences[onBoardingStatus] ?: false
+                statusOnBoarding
+            }
+    }
+}
