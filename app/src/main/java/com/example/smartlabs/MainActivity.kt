c959d5b02f94b5305a49c6655f0dbf62e96c01f0
Artem <dradinc@yandex.ru>
2023-04-25 11:34:25 +0300

    DataStore Preferences | working OnBoardingScreen

--- app/src/main/java/com/example/smartlabs/MainActivity.kt
+++ app/src/main/java/com/example/smartlabs/MainActivity.kt
@@ -3,17 +3,37 @@ package com.example.smartlabs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import com.example.smartlabs.Common.OnBoardingStatus
 import com.example.smartlabs.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var dataStoreOnBoarding: OnBoardingStatus
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val onBoardingActivity = Intent(this, OnBoarding::class.java)
-        startActivity(onBoardingActivity)
+
+        dataStoreOnBoarding = OnBoardingStatus(this)
+        val onBoardingStatus = dataStoreOnBoarding.getOnBoardingStatus().asLiveData()
+        onBoardingStatus.observe(this) {onBoardStat ->
+            when(onBoardStat) {
+                true -> {
+
+                }
+                false -> {
+                    val onBoardingActivity = Intent(this@MainActivity, OnBoarding::class.java)
+                    startActivity(onBoardingActivity)
+                    lifecycleScope.launch { dataStoreOnBoarding.setOnBoardingStatus(true) }
+                }
+            }
+        }
+
+
     }
 }
